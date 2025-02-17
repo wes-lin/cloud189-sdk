9c440fa0607de59972955b894cb21694bc59322b
Wesl <wes.lin@ehealth.com>
2025-02-17 16:01:40 +0800

    优化代码结构

--- test/app.js
+++ test/app.js
@@ -1,26 +1,24 @@
-const { CookieJar } = require("tough-cookie");
-const { CloudClient } = require("../dist");
+const { CookieJar } = require('tough-cookie')
+const { CloudClient } = require('../dist')
 
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-(async () => {
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+;(async () => {
   const cookies = [
-    "JSESSIONID=*******; Path=/; HttpOnly; hostOnly=true; aAge=2ms; cAge=20ms",
-    "COOKIE_LOGIN_USER=*******; Domain=cloud.189.cn; Path=/; HttpOnly; hostOnly=false; aAge=2ms; cAge=19ms",
-  ];
-  const cookieJar = new CookieJar();
-  cookies.forEach((cookie) =>
-    cookieJar.setCookieSync(cookie, "https://cloud.189.cn")
-  );
+    'JSESSIONID=*******; Path=/; HttpOnly; hostOnly=true; aAge=2ms; cAge=20ms',
+    'COOKIE_LOGIN_USER=*******; Domain=cloud.189.cn; Path=/; HttpOnly; hostOnly=false; aAge=2ms; cAge=19ms'
+  ]
+  const cookieJar = new CookieJar()
+  cookies.forEach((cookie) => cookieJar.setCookieSync(cookie, 'https://cloud.189.cn'))
 
-  const client = new CloudClient("******", "******", {
-    accessToken: "******",
-    cookieJar,
-  });
+  const client = new CloudClient('******', '******', {
+    accessToken: '******',
+    cookieJar
+  })
   // await client.login();
-  const t1 = await client.userSign();
-  console.log(t1);
-  const { familyInfoResp } = await client.getFamilyList();
-  console.log(familyInfoResp);
+  const t1 = await client.userSign()
+  console.log(t1)
+  const { familyInfoResp } = await client.getFamilyList()
+  console.log(familyInfoResp)
   // if (familyInfoResp) {
   //   for (let index = 0; index < familyInfoResp.length; index += 1) {
   //     const { familyId } = familyInfoResp[index];
@@ -32,6 +30,6 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   //     }
   //   }
   // }
-  const info = await client.getUserSizeInfo();
-  console.log(info);
-})();
+  const info = await client.getUserSizeInfo()
+  console.log(info)
+})()
