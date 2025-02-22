fe425ffdbcbc11fbb484a22377fdc39dafc70144
Wesl <wes.lin@ehealth.com>
2025-02-22 22:46:02 +0800

    修改配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,22 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import mixTheme from 'vuepress-theme-mix'
 import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
   bundler: viteBundler(),
-  theme: mixTheme({})
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: 'API',
+        link: '/api/'
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com'
+      }
+    ]
+  }),
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点'
 })
